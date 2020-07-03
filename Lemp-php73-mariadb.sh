048050a3ae8fc135367b0cc29c3b5d196b527727
Coeur Digital <53832698+Coeur-Digital@users.noreply.github.com>
2020-07-03 12:18:23 +0700

    Clean

--- Lemp-php73-mariadb.sh
+++ Lemp-php73-mariadb.sh
@@ -28,41 +28,41 @@ greeting() {
 
 update() {
 
-	sudo apt update;
+	sudo apt update
 	check_exit_status
 
-	sudo apt upgrade -y;
+	sudo apt upgrade -y
 	check_exit_status
 
 }
 
 nginx() {
 
-	sudo apt install nginx -y;
+	sudo apt install nginx -y
 	check_exit_status
-	sudo systemctl enable nginx;
+	sudo systemctl enable nginx
 	check_exit_status
-	sudo systemctl start nginx;
+	sudo systemctl start nginx
 	check_exit_status
-	sudo systemctl status nginx;
+	sudo systemctl status nginx
 
 	check_exit_status
 }
 mariadb() {
-	sudo apt install mariadb-server mariadb-client -y;
+	sudo apt install mariadb-server mariadb-client -y
 	check_exit_status
 
-	systemctl status mariadb;
+	systemctl status mariadb
 	check_exit_status
-	sudo systemctl enable mariadb;
+	sudo systemctl enable mariadb
 	check_exit_status
-	
+
 }
 php73() {
-sudo apt-get install software-properties-common -y;
-sudo add-apt-repository ppa:ondrej/php -y;
-sudo apt-get update;
-check_exit_status
+	sudo apt-get install software-properties-common -y
+	sudo add-apt-repository ppa:ondrej/php -y
+	sudo apt-get update
+	check_exit_status
 	sudo apt install php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline php7.3-mbstring php7.3-xml php7.3-gd php7.3-curl -y
 	check_exit_status
 	sudo systemctl enable php7.3-fpm
